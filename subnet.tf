3a2f7c610f28a3041fe14bc0e3aae8e36fce0fde
Dharmik Mashru <dharmik.mashru@techholding.co>
2025-01-22 10:29:24 +0530

    feat: adding condition to create dual stack subnets seperately

--- subnet.tf
+++ subnet.tf
@@ -3,64 +3,6 @@
 ########################
 
 #------------------------------------------------------------------------------------
-# Locals
-#------------------------------------------------------------------------------------
-locals {
-  public_subnets = flatten([
-    for index, subnet in var.public_subnets : {
-      cidr_block        = subnet.cidr_block
-      ipv6_cidr_block   = subnet.ipv6_cidr_block
-      availability_zone = element(var.availability_zones, index)
-    }
-  ])
-
-  private_subnets = flatten([
-    for index, subnet in var.private_subnets : {
-      cidr_block        = subnet.cidr_block
-      ipv6_cidr_block   = subnet.ipv6_cidr_block
-      availability_zone = element(var.availability_zones, index)
-    }
-  ])
-
-  nacl_ingress = flatten([
-    for idx, rule in var.nacl_ingress : [
-      for index in range(length(rule.cidr_blocks)) : {
-        from_port   = try(lookup(rule, "from_port"), lookup(rule, "port"))
-        to_port     = try(lookup(rule, "to_port"), lookup(rule, "port"))
-        rule_action = lookup(rule, "rule_action", "allow")
-        protocol    = lookup(rule, "protocol", "-1")
-        cidr_block  = tolist(rule.cidr_blocks)[index]
-        rule_number = ((idx + 2) * 100) + index
-        egress      = false
-      }
-    ]
-  ])
-
-  nacl_egress = flatten([
-    for idx, rule in var.nacl_egress : [
-      for index in range(length(rule.cidr_blocks)) : {
-        from_port   = try(lookup(rule, "from_port"), lookup(rule, "port"))
-        to_port     = try(lookup(rule, "to_port"), lookup(rule, "port"))
-        rule_action = lookup(rule, "rule_action", "allow")
-        protocol    = lookup(rule, "protocol", "-1")
-        cidr_block  = tolist(rule.cidr_blocks)[index]
-        rule_number = ((idx + 2) * 100) + index
-        egress      = true
-      }
-    ]
-  ])
-
-  public_subnet_ids                 = [for subnet in aws_subnet.public_subnet : subnet.id]
-  public_subnet_availability_zones  = [for subnet in aws_subnet.public_subnet : subnet.availability_zone]
-  public_subnet_cidr_blocks         = [for subnet in aws_subnet.public_subnet : subnet.cidr_block]
-  public_subnet_ipv6_cidr_blocks    = [for subnet in aws_subnet.public_subnet : subnet.ipv6_cidr_block]
-  private_subnet_ids                = [for subnet in aws_subnet.private_subnet : subnet.id]
-  private_subnet_availability_zones = [for subnet in aws_subnet.private_subnet : subnet.availability_zone]
-  private_subnet_cidr_blocks        = [for subnet in aws_subnet.private_subnet : subnet.cidr_block]
-  private_subnet_ipv6_cidr_blocks   = [for subnet in aws_subnet.private_subnet : subnet.ipv6_cidr_block]
-}
-
-#------------------------------------------------------------------------------------
 # Public Subnets
 #------------------------------------------------------------------------------------
 resource "aws_subnet" "public_subnet" {
@@ -69,8 +11,34 @@ resource "aws_subnet" "public_subnet" {
   cidr_block        = each.value.cidr_block
   availability_zone = each.value.availability_zone
 
+  map_public_ip_on_launch = lookup(each.value, "map_public_ip_on_launch", true)
+  tags = merge(
+    {
+      Name = "${var.name}-public-${each.value.availability_zone}"
+    },
+    var.tags
+  )
+}
+
+## Public route table association
+resource "aws_route_table_association" "public_subnet" {
+  count          = length(keys(aws_subnet.public_subnet)) > 0 ? length(keys(aws_subnet.public_subnet)) : 0
+  subnet_id      = element([for subnet in aws_subnet.public_subnet : subnet.id], count.index)
+  route_table_id = element(var.public_route_table_ids, count.index)
+}
+
+#------------------------------------------------------------------------------------
+# Public Subnets Dual stack
+#------------------------------------------------------------------------------------
+resource "aws_subnet" "public_subnet_dual_stack" {
+  for_each          = { for subnet in local.public_subnets_dual_stack : "${subnet.availability_zone}-${subnet.cidr_block}" => subnet }
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+
+  ipv6_cidr_block = var.enable_ipv6 && lookup(each.value, "ipv6_cidr_block", null) != null ? each.value.ipv6_cidr_block : null
+
   enable_dns64                                   = var.enable_ipv6 ? var.enable_dns64 : false
-  ipv6_cidr_block                                = var.enable_ipv6 ? each.value.ipv6_cidr_block : null
   assign_ipv6_address_on_creation                = var.enable_ipv6 ? var.assign_ipv6_address_on_creation : false
   enable_resource_name_dns_a_record_on_launch    = var.enable_ipv6 ? var.enable_resource_name_dns_a_record_on_launch : false
   enable_resource_name_dns_aaaa_record_on_launch = var.enable_ipv6 ? var.enable_resource_name_dns_aaaa_record_on_launch : false
@@ -85,9 +53,9 @@ resource "aws_subnet" "public_subnet" {
 }
 
 ## Public route table association
-resource "aws_route_table_association" "public_subnet" {
-  count          = var.public_route_table_ids == null ? 0 : length(keys(aws_subnet.public_subnet))
-  subnet_id      = element([for subnet in aws_subnet.public_subnet : subnet.id], count.index)
+resource "aws_route_table_association" "public_subnet_dual_stack" {
+  count          = length(keys(aws_subnet.public_subnet_dual_stack)) > 0 ? length(keys(aws_subnet.public_subnet_dual_stack)) : 0
+  subnet_id      = element([for subnet in aws_subnet.public_subnet_dual_stack : subnet.id], count.index)
   route_table_id = element(var.public_route_table_ids, count.index)
 }
 
@@ -100,12 +68,6 @@ resource "aws_subnet" "private_subnet" {
   cidr_block        = each.value.cidr_block
   availability_zone = each.value.availability_zone
 
-  enable_dns64                                   = var.enable_ipv6 ? var.enable_dns64 : false
-  ipv6_cidr_block                                = var.enable_ipv6 ? each.value.ipv6_cidr_block : null
-  assign_ipv6_address_on_creation                = var.enable_ipv6 ? var.assign_ipv6_address_on_creation : false
-  enable_resource_name_dns_a_record_on_launch    = var.enable_ipv6 ? var.enable_resource_name_dns_a_record_on_launch : false
-  enable_resource_name_dns_aaaa_record_on_launch = var.enable_ipv6 ? var.enable_resource_name_dns_aaaa_record_on_launch : false
-
   map_public_ip_on_launch = lookup(each.value, "map_public_ip_on_launch", false)
   tags = merge(
     {
@@ -117,49 +79,39 @@ resource "aws_subnet" "private_subnet" {
 
 ## Private route table association
 resource "aws_route_table_association" "private_subnet" {
-  count          = var.private_route_table_ids == null ? 0 : length(keys(aws_subnet.private_subnet))
+  count          = length(keys(aws_subnet.private_subnet)) > 0 ? length(keys(aws_subnet.private_subnet)) : 0
   subnet_id      = element([for subnet in aws_subnet.private_subnet : subnet.id], count.index)
   route_table_id = element(var.private_route_table_ids, count.index)
 }
 
-## Network ACL
-resource "aws_network_acl" "nacl" {
-  count  = var.create_acl ? 1 : 0
-  vpc_id = var.vpc_id
-  tags = {
-    Name = var.name
-  }
+#------------------------------------------------------------------------------------
+# Private Subnets Dual stack
+#------------------------------------------------------------------------------------
+resource "aws_subnet" "private_subnet_dual_stack" {
+  for_each          = { for subnet in local.private_subnets_dual_stack : "${subnet.availability_zone}-${subnet.cidr_block}" => subnet }
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+
+  ipv6_cidr_block = var.enable_ipv6 && lookup(each.value, "ipv6_cidr_block", null) != null ? each.value.ipv6_cidr_block : null
+
+  enable_dns64                                   = var.enable_ipv6 ? var.enable_dns64 : false
+  assign_ipv6_address_on_creation                = var.enable_ipv6 ? var.assign_ipv6_address_on_creation : false
+  enable_resource_name_dns_a_record_on_launch    = var.enable_ipv6 ? var.enable_resource_name_dns_a_record_on_launch : false
+  enable_resource_name_dns_aaaa_record_on_launch = var.enable_ipv6 ? var.enable_resource_name_dns_aaaa_record_on_launch : false
+
+  map_public_ip_on_launch = lookup(each.value, "map_public_ip_on_launch", true)
+  tags = merge(
+    {
+      Name = "${var.name}-private-${each.value.availability_zone}"
+    },
+    var.tags
+  )
 }
 
-## Network ACL association
-resource "aws_network_acl_association" "nacl_association" {
-  for_each       = var.create_acl ? { for subnet in aws_subnet.private_subnet : "${subnet.availability_zone}-${subnet.cidr_block}" => subnet } : {}
-  subnet_id      = each.value.id
-  network_acl_id = aws_network_acl.nacl[0].id
-}
-
-## Network ACL rule for inbound traffic
-resource "aws_network_acl_rule" "ingress_nacl_rule" {
-  for_each       = var.create_acl ? { for index, rule in local.nacl_ingress : "${var.name}-${rule.rule_number}-${rule.egress}" => rule } : {}
-  network_acl_id = aws_network_acl.nacl[0].id
-  rule_number    = lookup(each.value, "rule_number")
-  egress         = lookup(each.value, "egress")
-  protocol       = lookup(each.value, "protocol")
-  rule_action    = lookup(each.value, "rule_action")
-  cidr_block     = lookup(each.value, "cidr_block")
-  from_port      = lookup(each.value, "from_port")
-  to_port        = lookup(each.value, "to_port")
-}
-
-## Network ACL rule for outbound traffic
-resource "aws_network_acl_rule" "egress_nacl_rule" {
-  for_each       = var.create_acl ? { for index, rule in local.nacl_egress : "${var.name}-${rule.rule_number}-${rule.egress}" => rule } : {}
-  network_acl_id = aws_network_acl.nacl[0].id
-  rule_number    = lookup(each.value, "rule_number")
-  egress         = lookup(each.value, "egress")
-  protocol       = lookup(each.value, "protocol")
-  rule_action    = lookup(each.value, "rule_action")
-  cidr_block     = lookup(each.value, "cidr_block")
-  from_port      = lookup(each.value, "from_port")
-  to_port        = lookup(each.value, "to_port")
+## Public route table association
+resource "aws_route_table_association" "private_subnet_dual_stack" {
+  count          = length(keys(aws_subnet.private_subnet_dual_stack)) > 0 ? length(keys(aws_subnet.private_subnet_dual_stack)) : 0
+  subnet_id      = element([for subnet in aws_subnet.private_subnet_dual_stack : subnet.id], count.index)
+  route_table_id = element(var.private_route_table_ids, count.index)
 }
